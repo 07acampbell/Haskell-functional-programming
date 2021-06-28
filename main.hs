9905e8bd772ed26f91ddac8828dcb6526dc4a296
arancampb3ll <70891862+07acampbell@users.noreply.github.com>
2021-06-28 14:59:13 +0000

    did things that make it turing complete

--- main.hs
+++ main.hs
@@ -1,1 +1,15 @@
-main = putStrLn "Hello World"
+bigger a b = if a > b then a else b
+bigger3 a b c = bigger (bigger a b) c
+
+triangular 1 = 1
+triangular n = n + triangular(n-1)
+
+linsearch :: Eq a => [a] -> a -> bool
+linsearch [] _ = false
+search(x:xs) y = x==y || search xs y
+
+
+main :: IO ()
+main = do
+
+print(triangular 4)
