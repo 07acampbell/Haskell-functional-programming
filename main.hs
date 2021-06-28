5aac0b6bff958eb52bbf9502ce85278876bcec0d
arancampb3ll <70891862+07acampbell@users.noreply.github.com>
2021-06-28 13:16:55 +0000

    changes

--- main.hs
+++ main.hs
@@ -1,9 +1,1 @@
-fizz :: Int -> String
-fizz n | n `mod` 15 == 0  = "FizzBuzz"
-       | n `mod` 3  == 0  = "Fizz"
-       | n `mod` 5  == 0  = "Buzz"
-       | n `mod` 11 == 0  = "bruh,fart?"
-       | otherwise = show n
-
-main :: IO()
-main = mapM_ putStrLn $ map fizz [1..100]
+main = putStrLn "Hello World"
